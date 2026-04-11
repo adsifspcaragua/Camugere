3bbac3518d7df374147ac17bd2565a0fb12a71d2
RAFAEL TOMAZ DE OLIVEIRA AQUINO <CG3033791@car.ifsp>
2026-04-10 22:01:51 -0300

    FirstCommit

---
+++ api/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: "Funcionou"
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Servirdor Rodando no http://localhost:3000')
+})
